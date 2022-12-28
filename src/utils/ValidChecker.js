23f7d88ca05db68e1a26b98d9cc2f5b7c9818341
Aashish Gubhaju <aashishgubhaju12@gmail.com>
2022-12-28 10:31:58 -0700

    add the utils
    check brackets app'

---
+++ src/utils/ValidChecker.js
@@ -0,0 +1,25 @@
+function ValidChecker(str) {
+  const arr = [];
+  console.log(arr);
+
+  const mappedPairs = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  for (const element of str) {
+    if (element === "(" || element === "{" || element === "[") {
+      arr.push(element);
+    } else {
+      const lastBracket = arr.pop();
+      if (element !== mappedPairs[lastBracket]) {
+        return "not valid.";
+      }
+    }
+  }
+  if (str.length !== 0) {
+    return "valid";
+  }
+}
+
+export default ValidChecker;
